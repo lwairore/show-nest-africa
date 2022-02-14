d7a8d00481a81fd5b7329f8b6faf9cf33c958cc5
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 21:48:18 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'snap-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'show-nest-africa';
+}
