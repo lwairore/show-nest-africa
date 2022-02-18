5591d116b6e4de58af34c160cc7cf05fd6b4f7cc
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-18 16:56:04 +0300

    - Generate module 'SinglesModule' with routing support.

---
+++ src/app/events/singles/singles.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SinglesRoutingModule } from './singles-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SinglesRoutingModule
+  ]
+})
+export class SinglesModule { }
