262b3bad61cb537aa7c038e6cbaae92fc7357e3c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-18 16:16:18 +0300

    - Generate module 'PastMomentModule' with routing support.

---
+++ src/app/events/past-moment/past-moment.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PastMomentRoutingModule } from './past-moment-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PastMomentRoutingModule
+  ]
+})
+export class PastMomentModule { }
