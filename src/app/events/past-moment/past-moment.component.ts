93aec7409066522b73b3fecdcc28d23047fc2e38
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-18 16:23:19 +0300

    - Generate component 'PastMomentComponent'.

---
+++ src/app/events/past-moment/past-moment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'snap-past-moment',
+  templateUrl: './past-moment.component.html',
+  styleUrls: ['./past-moment.component.css']
+})
+export class PastMomentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/events/past-moment/past-moment.module.ts
+++ src/app/events/past-moment/past-moment.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PastMomentRoutingModule } from './past-moment-routing.module';
+import { PastMomentComponent } from './past-moment.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PastMomentComponent],
   imports: [
     CommonModule,
     PastMomentRoutingModule
