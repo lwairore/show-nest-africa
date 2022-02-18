2228d1713ce62afce9bd4bb7c0d4476d8a3ee0d9
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-18 15:05:37 +0300

    - Map component to 'LoginComponent' to a 'path'.

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth.component';
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 
@@ -12,6 +13,10 @@ const routes: Routes = [
       {
         path: 'register',
         component: RegisterComponent,
+      },
+      {
+        path: 'log-in',
+        component: LoginComponent,
       },
     ],
   },
