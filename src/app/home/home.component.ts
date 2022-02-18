8e945ee1aceb73d29f91d2419d3b0796b262a711
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-18 11:34:45 +0300

    ..

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'snap-home',
@@ -7,6 +8,67 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class HomeComponent implements OnInit {
+  customOptionsForBanner: OwlOptions = {
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    nav: false,
+    dots: false,
+    // responsiveClass: true,
+
+    slideBy: 1,
+    responsive: {
+      0: {
+        items: 1
+      },
+      480: {
+        items: 1,
+        slideBy: 1,
+        nav: false,
+      },
+      991: {
+        items: 1,
+        slideBy: 1,
+        nav: false,
+      },
+      1024: {
+        items: 1,
+        slideBy: 1,
+        autoplay: true
+      }
+    },
+  }
+
+  customOptionsForVideos: OwlOptions = {
+    loop: false,
+    margin: 30,
+    autoplay: false,
+    nav: false,
+    dots: false,
+    // responsiveClass: true,
+items: 3,
+    slideBy: 1,
+    // responsive: {
+    //   0: {
+    //     items: 1
+    //   },
+    //   480: {
+    //     items: 1,
+    //     slideBy: 1,
+    //     nav: false,
+    //   },
+    //   991: {
+    //     items: 4,
+    //     slideBy: 1,
+    //     nav: false,
+    //   },
+    //   1024: {
+    //     items: 4,
+    //     slideBy: 1,
+    //     autoplay: true
+    //   }
+    // },
+  }
 
   constructor() { }
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    CarouselModule,
   ]
 })
 export class HomeModule { }
