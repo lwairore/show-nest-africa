6becbe82eea67cb3ea5ddf483a12d758eff22e3b
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-18 16:24:33 +0300

    - Lazy load component 'PastMomentComponent'.

--- src/app/events/past-moment/past-moment-routing.module.ts
+++ src/app/events/past-moment/past-moment-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PastMomentComponent } from './past-moment.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PastMomentComponent,
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
