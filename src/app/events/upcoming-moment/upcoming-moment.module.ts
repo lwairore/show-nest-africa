bc63be365cbfe843e4fab83abc0332dab22f83fc
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-18 16:43:11 +0300

    - Generate module 'UpcomingMomentModule' with routing support.

---
+++ src/app/events/upcoming-moment/upcoming-moment.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UpcomingMomentRoutingModule } from './upcoming-moment-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    UpcomingMomentRoutingModule
+  ]
+})
+export class UpcomingMomentModule { }
