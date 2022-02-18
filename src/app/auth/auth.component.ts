a942bb9db93e76efa393abc1590fc0e0eb410d39
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-18 13:32:29 +0300

    - Generate component 'AuthComponent'.

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'snap-auth',
+  templateUrl: './auth.component.html',
+  styles: [
+  ]
+})
+export class AuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
