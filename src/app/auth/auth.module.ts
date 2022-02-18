0efdafcf7aa7d123bd30acdded17d35b0ac41d4d
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-18 15:04:20 +0300

    - Generate component 'LoginComponent'.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [AuthComponent, RegisterComponent],
+  declarations: [AuthComponent, RegisterComponent, LoginComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
