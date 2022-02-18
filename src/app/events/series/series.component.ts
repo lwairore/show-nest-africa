d2fec2ff13e4335eef8e3c866e9d43f6a6f2217b
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-18 17:18:33 +0300

    - Generate component 'SeriesComponent'.

---
+++ src/app/events/series/series.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'snap-series',
+  templateUrl: './series.component.html',
+  styles: [
+  ]
+})
+export class SeriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/events/series/series.module.ts
+++ src/app/events/series/series.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SeriesRoutingModule } from './series-routing.module';
+import { SeriesComponent } from './series.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SeriesComponent],
   imports: [
     CommonModule,
     SeriesRoutingModule
