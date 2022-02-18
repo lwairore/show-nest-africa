608e34452ae0671323d1b0783b0a5584ee34fc38
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-18 14:50:47 +0300

    - Map component 'RegisterComponent' to a 'path'.

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth.component';
+import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: AuthComponent,
-    children: [],
+    children: [
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+    ],
   },
 ];
 
