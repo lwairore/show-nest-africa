be11061f1b87f637e867d3b666541d66e839c2fa
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-18 17:07:35 +0300

    - Generate module 'SeriesModule' with routing support.

---
+++ src/app/events/series/series.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SeriesRoutingModule } from './series-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SeriesRoutingModule
+  ]
+})
+export class SeriesModule { }
