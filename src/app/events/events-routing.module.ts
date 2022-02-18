6280a42270118ff6a3d7df02e3bf1685e1557b1f
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-18 16:19:37 +0300

    - Map component 'EventsComponent' to a 'path'.

--- src/app/events/events-routing.module.ts
+++ src/app/events/events-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventsComponent } from './events.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: EventsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
