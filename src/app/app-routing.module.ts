97305ea56c3a1dc356f47c909c7a8695c8569948
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-18 15:29:03 +0300

    - Add package 'angular-deploy'.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,18 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./home/home.module')
-      .then(h => h.HomeModule),
+    // loadChildren: () => import('./home/home.module')
+    //   .then(h => h.HomeModule),
+    redirectTo: 'auth',
   },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module')
       .then(a => a.AuthModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'auth',
   },
 ];
 
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -25,6 +25,10 @@ const routes: Routes = [
       },
     ],
   },
+  {
+    path: '**',
+    redirectTo: 'log-in',
+  },
 ];
 
 @NgModule({
