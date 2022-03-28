5267acbfd34043d753c96be464121c205b317386
Karangu Lucas Wairore <kwairore@gmail.com>
2022-03-28 10:21:08 +0300

    - Update type 'PastEventFormatHttpResponse'.

--- src/app/shared/custom-types/past-event-format-http-response.ts
+++ src/app/shared/custom-types/past-event-format-http-response.ts
@@ -1,7 +1,9 @@
-import { ProposalFormatHttpResponse } from "./proposal-format-http-response";
+import { ImageItemPreviewFormatHttpResponse } from "./image-item-preview-format-http-response";
 
 export type PastEventFormatHttpResponse = Readonly<{
     endsOn: string;
     id: number;
-    proposal: ProposalFormatHttpResponse;
+    username: string;
+    nameOfMoment: string;
+    poster: ImageItemPreviewFormatHttpResponse;
 }>
