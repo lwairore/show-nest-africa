7600a9329eb448e88d20a8b8f222305d79ab674a
Karangu Lucas Wairore <kwairore@gmail.com>
2022-03-28 10:19:48 +0300

    - Remove type 'ProposalFormatHttpResponse'.

--- src/app/shared/custom-types/proposal-format-http-response.ts
+++
@@ -1,7 +0,0 @@
-import { ImageItemPreviewFormatHttpResponse } from "@sharedModule/custom-types";
-
-export type ProposalFormatHttpResponse = Readonly<{
-    username: string;
-    nameOfMoment: string;
-    poster: ImageItemPreviewFormatHttpResponse;
-}>
